80ac10ec51bc537b16ede429c41f5689b0366339
dounia-elg <dounia.elg20@gmail.com>
2025-11-24 06:58:24 +0100

    feat: update Register component with enhanced form fields and layout

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-    basseURL: "http://localhost:3001/api/v1",
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL: "http://localhost:3001/api/v1",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default API;
